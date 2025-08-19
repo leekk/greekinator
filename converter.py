35a9acb65bb031162b9cbc44b0344a704ce55c90
Eleanor <157729330+leekk@users.noreply.github.com>
2025-08-19 15:04:21 -0400

    Update converter.py

--- converter.py
+++ converter.py
@@ -42,8 +42,8 @@ st.markdown(
     }
     .hero-text-1 {
         position: absolute;
-        top: 30%;
-        left: 50%;
+        top: 20%;
+        left: 20%;
         transform: (-50%, -50%);
         color: white;
         font-size: 3em;
