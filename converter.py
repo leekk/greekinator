fcb1f4ccad2bf4903ceeaa7a558f049dae6d9fa6
vincentdesr2-cpu <vincentdesr2@gmail.com>
2025-08-20 20:09:34 -0400

    Update converter.py

--- converter.py
+++ converter.py
@@ -667,7 +667,7 @@ def rootsGuesser(word):
     if last_thing in allLiquidConsonants:
         rootStatus = "liquidStem"
     
-    st.write("Enter the root of your verb's first principle part")
+    st.write("Enter the root of your verb's first principle part (for more information enter '?')")
     st.write(last_thing)
     if word = "?"
         st.write("Whilst the principle part is created by adding a verb ending to the stem, the stem is created by adding one or multiple affixes to the root.")
